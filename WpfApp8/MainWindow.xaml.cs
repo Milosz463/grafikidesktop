437918ccbe1b710274644cb70e50a8a20e0b19ff
Milosz463 <miloszdawiec6@gmail.com>
2024-12-04 13:40:01 +0100

    awfafwaf

--- WpfApp8/MainWindow.xaml.cs
+++ WpfApp8/MainWindow.xaml.cs
@@ -24,5 +24,22 @@ namespace WpfApp8
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            image.Source = new BitmapImage(new Uri("sigma2.jpg",UriKind.Relative));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<string> listaObrazkow = new List<string>();
+            listaObrazkow.Add("sigma.jpg");
+            listaObrazkow.Add("sigma2.jpg");
+            listaObrazkow.Add("sigma3.jpg");
+            listaObrazkow.Add("sigma4.jpg");
+            Random random= new Random();
+            int indekst = random.Next(listaObrazkow.Count);
+            image.Source = new BitmapImage(new Uri(listaObrazkow[indekst], UriKind.Relative));
+        }
     }
 }
